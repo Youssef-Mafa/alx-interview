58fc8d9c8cf7034cc088b9d2a0bcef0ff2881a17
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-10-24 18:08:14 +0100

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,55 +1,38 @@
 #!/usr/bin/python3
 """
-Script that reads stdin line by line and computes metrics.
+    script that reads stdin line by line and computes metrics
 """
 
 import sys
 
 if __name__ == "__main__":
-    total_file_size = 0
-    count = 0
-    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {code: 0 for code in status_codes}
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-    def print_stats(stats: dict, total_file_size: int) -> None:
-        """Print the statistics"""
-        print("File size: {:d}".format(total_file_size))
-        for code, value in sorted(stats.items()):
-            if value > 0:
-                print("{}: {}".format(code, value))
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
             count += 1
             data = line.split()
-            
-
-            if len(data) < 7:
-                continue
-            
-
             try:
                 status_code = data[-2]
-                file_size = int(data[-1])
-
-
-                total_file_size += file_size
-
-
                 if status_code in stats:
                     stats[status_code] += 1
-
-            except (ValueError, IndexError):
-                continue  
-            
-
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
             if count % 10 == 0:
-                print_stats(stats, total_file_size)
-
-
-        print_stats(stats, total_file_size)
-
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-
-        print_stats(stats, total_file_size)
-        raise  
+        print_stats(stats, filesize)
+        raise
