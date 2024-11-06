8d2391c5769faa48f6f54a52616022078cfe01a1
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-11-06 11:49:10 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,44 +1,49 @@
 #!/usr/bin/python3
-""" N queens """
+"""
+The N queens puzzle is the challenge of placing N non-attacking queens
+"""
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
-    print("Usage: nqueens N")
-    exit(1)
+def solveNQueens(N):
+    """Solve N queens"""
+    col = []
+    post_diag = []  # row + col
+    neg_diag = []  # row - col
+    res = []
 
-if not sys.argv[1].isdigit():
-    print("N must be a number")
-    exit(1)
+    def solve(row):
+        """Solve"""
+        if row == N:
+            res.append([[i, col[i]] for i in range(N)])
+            return
+        for i in range(N):
+            if (i not in col and row + i
+                    not in post_diag and row - i not in neg_diag):
+                col.append(i)
+                post_diag.append(row + i)
+                neg_diag.append(row - i)
+                solve(row + 1)
+                col.pop()
+                post_diag.pop()
+                neg_diag.pop()
 
-if int(sys.argv[1]) < 4:
-    print("N must be at least 4")
-    exit(1)
-
-n = int(sys.argv[1])
-
-
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
-    if i < n:
-        for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
-    else:
-        yield a
-
-
-def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
-        k = []
-        i = 0
+    solve(0)
+    return res
 
 
-solve(n)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    if int(sys.argv[1]) < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    N = int(sys.argv[1])
+
+    res = solveNQueens(N)
+    for i in res:
+        print(i)
