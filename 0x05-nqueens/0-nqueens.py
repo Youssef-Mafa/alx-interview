79bee419e0d895df8eb94d4615e92a9ea45b13c0
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-11-06 11:48:03 +0100

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,36 +1,44 @@
 #!/usr/bin/python3
-"""
-N-Queens Puzzle Solution
-Solves for N non-attacking queens in an NxN chessboard.
-"""
+""" N queens """
 import sys
 
-def queens(N, row=0, col_set=set(), pos_diag_set=set(), neg_diag_set=set()):
 
-    if row == N:
-        yield [[r, c] for r, c in enumerate(col_set)]
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
+
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
+
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
     else:
-        for col in range(N):
-            pos_diag = row + col
-            neg_diag = row - col
-            if col not in col_set and pos_diag not in pos_diag_set and neg_diag not in neg_diag_set:
-                yield from queens(N, row + 1, col_set | {col}, pos_diag_set | {pos_diag}, neg_diag_set | {neg_diag})
+        yield a
 
-def main():
 
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        sys.exit(1)
-    N = int(sys.argv[1])
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
 
-    for solution in queens(N):
-        print(solution)
 
-if __name__ == "__main__":
-    main()
+solve(n)
