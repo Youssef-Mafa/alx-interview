2cd14645463e813a665f7d0b2b625b2bb36fdb10
Youssef-Mafa <125815717+Youssef-Mafa@users.noreply.github.com>
2024-11-29 15:26:01 +0100

    Update 0-making_change.py

--- 0x08-making_change /0-making_change.py
+++ 0x08-making_change /0-making_change.py
@@ -1,27 +1,24 @@
 #!/usr/bin/python3
-"""Optimized Making Change Algorithm"""
+"""Implement Making Change Algorithm"""
+
+
 def makeChange(coins, total):
-    """
-    Calculate fewest number of coins needed to meet total
-    Args:
-        coins: list of coin values
-        total: target sum
-    Returns:
-        Number of coins needed, -1 if impossible, 0 if total <= 0
-    """
-    if not coins or coins is None:
-        return -1
+    """ Generate changes needed to reach total"""
     if total <= 0:
         return 0
-        
+
+    check, steps = 0, 0
     coins.sort(reverse=True)
-    change = 0
-    
+
     for coin in coins:
-        while coin <= total:
-            total -= coin
-            change += 1
-        if total == 0:
-            return change
-            
+        while check < total:
+            check += coin
+            steps += 1
+
+        if check == total:
+            return steps
+
+        check -= coin
+        steps -= 1
+
     return -1
